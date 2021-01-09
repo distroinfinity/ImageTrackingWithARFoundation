9f14133a9b7161cee19ab10fcd22b138f6b237ea
Manu Rajput <manurajput2911@gmail.com>
2021-01-09 16:22:06 +0530

    First and Final commit

---
+++ Assets/Scripts/planeMatManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class planeMatManager : MonoBehaviour
+{
+
+    public Material planeMat;
+    public Button[] planeTexButton;
+
+     void Awake(){
+        foreach(var b in planeTexButton){
+            Texture tex = b.transform.Find("Mask/RawImage").GetComponent<RawImage>().texture;
+            b.onClick.AddListener(() => OnClickButton(tex));
+        }
+    }
+    void OnClickButton(Texture tex){
+        planeMat.mainTexture = tex;
+    }
+}
